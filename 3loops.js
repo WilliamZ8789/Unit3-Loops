e463289e568e69ab50b9f6211a6e7b141a0fe51d
WilliamZ8789 <williamz100@nycstudents.net>
2026-09-24 11:58:13 -0400

    3

---
+++ 3loops.js
@@ -0,0 +1,16 @@
+//P1 - Range Builder
+function
+
+//P2 - Sum a Range
+
+
+//P3 - Countdown
+
+
+//P4 - Count the Vowels
+
+
+//P5 - Multiplication Table
+
+
+//P6 - Primes Under a Limit
---
+++ app3.js
@@ -0,0 +1,21 @@
+/* let x = 100
+// For loop
+for (let i = 0; i < 4; i++) {
+    x = x - i;
+}
+console.log(x)
+
+// While loop
+while (x > 0) {
+    x = x - 10;
+    console.log(x)
+} */
+
+//Array
+const students = ["Angel", "Derek", "Brian", "Vanessa", "Eric"];
+console.log(students[i])
+
+for (let i = 0; i < students.length; i++) {
+    if (students[i] === "Brian")
+        console.log(students[i], "We have found Brian?")
+}
